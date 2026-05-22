25f6e46cb2cdbd415797609d5c0fcc9696dd38a8
braxton <braxtonjonesttsl@gmail.com>
2026-05-22 00:07:41 -0400

    intial commit

---
+++ apps/apricity-desktop/src/App.tsx
@@ -0,0 +1,3 @@
+export default function ApricityApp() {
+  return <main></main>;
+}
---
+++ apps/apricity-desktop/src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./App.css";
+import ApricityApp from "./App";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ApricityApp />
+  </React.StrictMode>,
+);
